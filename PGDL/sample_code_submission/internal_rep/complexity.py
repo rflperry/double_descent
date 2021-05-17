69d2bb7ec0a4c30c0c9572518b7abf2d8204bd19
rflperry <rflperry@gmail.com>
2021-05-17 18:25:01 -0400

    speedups

--- PGDL/sample_code_submission/internal_rep/complexity.py
+++ PGDL/sample_code_submission/internal_rep/complexity.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from tensorflow import keras
 from collections import defaultdict
 
+import numba
+
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 from scipy.stats import * 
@@ -39,7 +41,6 @@ def complexity(model, dataset, program_dir, measure = 'KF-ratio'):
 		
 	print('-------Final Scores---------', complexityScore)
 	return complexityScore
-
 
 
 
@@ -94,13 +95,14 @@ def get_polytope(model, dataset, computeOver=500, batchSize=50):
 	polytope_memberships_list = []
 
 	# for batch in batches:
-	for x, y in dataset.batch(500):
+	for x, y in dataset.batch(500): # parallelize
 
 		batch_ = x
+		n_prior_relus = 0
 		
 		with tf.GradientTape(persistent=True) as tape:
 			intermediateVal = [batch_]
-			polytope_memberships = []
+			polytope_memberships = np.zeros((len(x)))
 			last_activations = batch_
 			tape.watch(last_activations)
 			for l, layer_ in enumerate(model.layers):
@@ -109,13 +111,36 @@ def get_polytope(model, dataset, computeOver=500, batchSize=50):
 
 				preactivation = layer_(last_activations)
 				binary_preactivation = (K.cast((preactivation > 0), "float"))
-				polytope_memberships.append( np.array(binary_preactivation).reshape(len(x), -1))
+				
+				n_layer_relus = np.product(binary_preactivation.shape[1:])
+
+				polytope_memberships += np.tensordot(
+					np.array(binary_preactivation).reshape(len(x), -1),
+					2 ** (np.arange(0, n_layer_relus) + n_prior_relus),
+					axes=1,
+				)
+
+				n_prior_relus += n_layer_relus
+
+				# polytope_memberships.append(
+				# 	np.unique(
+				# 		np.array(binary_preactivation).reshape(len(x), -1),
+				# 		axis=0,
+				# 		return_inverse=True)[1].reshape(len(x), -1))
 				last_activations = preactivation * binary_preactivation
-			
-		polytope_memberships = [np.tensordot(np.concatenate(polytope_memberships, axis = 1), 2 ** np.arange(0, np.shape(np.concatenate(polytope_memberships, axis = 1))[1]), axes = 1)]
-		polytope_memberships_list.append(polytope_memberships[0])
-	
-	poly_m = np.hstack(polytope_memberships_list)
+
+		# polytope_memberships = np.tensordot(
+		# 	np.concatenate(polytope_memberships, axis = 1),
+		# 	2 ** np.arange(0, np.shape(np.concatenate(polytope_memberships, axis = 1))[1]),
+		# 	axes = 1)
+		polytope_memberships_list.append(polytope_memberships)
+		# polytope_memberships_list.append(np.unique(
+		# 				np.hstack(polytope_memberships),
+		# 				axis=0,
+		# 				return_inverse=True)[1])
+
+
+	poly_m = np.unique(np.concatenate(polytope_memberships_list), return_inverse=True)[1]
 	return poly_m
 
 
--- PGDL/sample_code_submission/internal_rep/matrix_funcs.py
+++ PGDL/sample_code_submission/internal_rep/matrix_funcs.py
@@ -9,6 +9,7 @@ import community
 from community import community_louvain
 
 import pandas as pd
+import numba
 
 import copy
 
@@ -19,30 +20,17 @@ def ger_matrix_from_poly(model, dataset, poly_m):
 
     unique_poly = np.unique(poly_m)
     n_poly = len(unique_poly)
-
     # for key in L_matrices:
     L_mat = np.zeros((len(poly_m), n_poly))
-    for idx, poly_i in enumerate(poly_m):
-        poly_idx = np.where(unique_poly==poly_i)
-        L_mat[idx, poly_idx] = pred_y[idx]+1
-            # if key == '0/1':
-            #     L_mat[idx, poly_idx] = pred_label[idx]
-            # elif key == 'true_label':
-            #     L_mat[idx, poly_idx] = 2*y_train[idx]-1
-            # elif key == 'est_label':
-            #     L_mat[idx, poly_idx] = 2*pred_label[idx]-1
-            # elif key == 'est_poster':
-            #     L_mat[idx, poly_idx] = 2*pred_poster[idx]-1
-        # L_matrices[key].append(L_mat)
 
-    # gen_gap = abs((1-test_acc) - (1-train_acc))
+    # We know that poly_m is shape=(n,) and values 0,...,k
+    L_mat[np.arange(len(poly_m)), poly_m] = pred_y + 1
+
     test_gen_err = (1-test_acc)
-    return np.array(L_mat), test_gen_err
+    return L_mat, test_gen_err
 
 
 def get_label_pred(model, dataset, computeOver=500, batchSize=50):
-
-    
     model.compile(optimizer='adam',
 			loss='sparse_categorical_crossentropy',
 			metrics=['accuracy'])
@@ -66,51 +54,58 @@ def get_label_pred(model, dataset, computeOver=500, batchSize=50):
 
 ##********** Matrix ranks *************##
 def get_stable_rank(m):
-  '''
-  Compute stable rank of a matrix: frobenius norm (squared) / spectral norm (squared)
-  '''
-  return norm(m, ord='fro')**2 /norm(m, ord=2)**2 
+    '''
+    Compute stable rank of a matrix: frobenius norm (squared) / spectral norm (squared)
+    '''
+    return norm(m, ord='fro')**2 / norm(m, ord=2)**2 
+
 
 def get_KF_Schatten_norms(m, num_k=5):
-  '''
-  Compute different matrix norms
-  Input: m - 2d matrix (n by L)
-  Return: 4 1D numpy arrays
-  - First: Ky-Fan results [Un-normalized], where k-th element is the sum of top-k singular values
-  - Second: Ky-Fan results [Normalized], where k-th element is the ratio of the variance explained by top-k singular values
-  - Third: Ky-Fan results on m^T @ m, where k-th element is the sum of top-k eigenvalues of m^T @ m (i.e., singular values of (m) squared)
-  - Fourth: Schatten results, where k-th element is the k-norm, (sum_i sigma^k)^{1/k}
-  '''
-  ss = np.linalg.svd(m, full_matrices=False, compute_uv=False)
-  KFs_raw = np.array( [ss[:i+1].sum() for i in range(num_k)])
-  total = ss.sum()
-  KFs = KFs_raw / total
-  evalues = ss**2
-  KFs_kernel = np.array( [evalues[:i+1].sum() for i in range(num_k)])
-  Schattens = [total]
-  ss_pow = copy.deepcopy(ss)
-  for i in range(2, num_k+1):
-    ss_pow = np.multiply(ss_pow, ss)
-    Schattens.append( np.power(ss_pow.sum(), 1/i))
-  return KFs_raw, KFs, KFs_kernel, np.array(Schattens)
+    '''
+    Compute different matrix norms
+    Input: m - 2d matrix (n by L)
+    Return: 4 1D numpy arrays
+    - First: Ky-Fan results [Un-normalized], where k-th element is the sum of top-k singular values
+    - Second: Ky-Fan results [Normalized], where k-th element is the ratio of the variance explained by top-k singular values
+    - Third: Ky-Fan results on m^T @ m, where k-th element is the sum of top-k eigenvalues of m^T @ m (i.e., singular values of (m) squared)
+    - Fourth: Schatten results, where k-th element is the k-norm, (sum_i sigma^k)^{1/k}
+    '''
+    ss = np.linalg.svd(m, full_matrices=False, compute_uv=False)
+    rank = np.sum(ss > (ss[0] * np.max(m.shape) * np.finfo(float).eps))
+    KFs_raw = np.array( [ss[:i+1].sum() for i in range(num_k)])
+    total = ss.sum()
+    KFs = KFs_raw / total
+    evalues = ss**2
+    KFs_kernel = np.array( [evalues[:i+1].sum() for i in range(num_k)])
+    Schattens = [total]
+    ss_pow = copy.deepcopy(ss)
+    for i in range(2, num_k+1):
+        ss_pow = np.multiply(ss_pow, ss)
+        if i == 2:
+            stable_rank = ss_pow.sum() / ss[0]**2
+        Schattens.append( np.power(ss_pow.sum(), 1/i))
+    if num_k < 2:
+        ss_pow = np.multiply(ss_pow, ss)
+        stable_rank = ss_pow.sum() / ss[0]**2
+    return KFs_raw, KFs, KFs_kernel, np.array(Schattens), rank, stable_rank
 
 
 def graph_metrics(m):
-  '''
-  Input: internal representation, n by L
-  Return: 2-tuple
-  - clustering coefficients of a bipartite graph built from m, a measure of local density of the connectivity
-  ref: https://networkx.org/documentation/stable//reference/algorithms/generated/networkx.algorithms.bipartite.cluster.clustering.html#networkx.algorithms.bipartite.cluster.clustering
-  - modularity: relative density of edges inside communities with respect to edges outside communities.
-  ref: https://python-louvain.readthedocs.io/en/latest/api.html#community.modularity
-  '''
-  sM = scipy.sparse.csr_matrix(m)
-  G = bipartite.matrix.from_biadjacency_matrix(sM)
-  avg_c = bipartite.average_clustering(G, mode="dot")
-  partition = community_louvain.best_partition(G)
-  modularity = community.modularity(partition, G)
+    '''
+    Input: internal representation, n by L
+    Return: 2-tuple
+    - clustering coefficients of a bipartite graph built from m, a measure of local density of the connectivity
+    ref: https://networkx.org/documentation/stable//reference/algorithms/generated/networkx.algorithms.bipartite.cluster.clustering.html#networkx.algorithms.bipartite.cluster.clustering
+    - modularity: relative density of edges inside communities with respect to edges outside communities.
+    ref: https://python-louvain.readthedocs.io/en/latest/api.html#community.modularity
+    '''
+    sM = scipy.sparse.csr_matrix(m)
+    G = bipartite.matrix.from_biadjacency_matrix(sM)
+    avg_c = bipartite.average_clustering(G, mode="dot")
+    partition = community_louvain.best_partition(G)
+    modularity = community.modularity(partition, G)
 
-  return avg_c, modularity
+    return avg_c, modularity
 
 
 def compute_complexity(X, k=5):
@@ -123,12 +118,14 @@ def compute_complexity(X, k=5):
 
 #   for i in range(len(X)):
     rep = X
-    plot_dict['ranks'].append(matrix_rank(rep))
-    plot_dict['stable_ranks'].append(get_stable_rank(rep))
+    KF_norms, KF_ratios, KF_kers, Schattens, rank, stable_rank = get_KF_Schatten_norms(rep, num_k=k)
+    plot_dict['ranks'].append(rank)
+    plot_dict['stable_ranks'].append(stable_rank)
+
     avg_c, modularity = graph_metrics(rep)
     plot_dict['avg_clusters'].append(avg_c)
     plot_dict['modularity'].append(modularity)
-    KF_norms, KF_ratios, KF_kers, Schattens = get_KF_Schatten_norms(rep, num_k=k)
+    
     plot_dict['KF-raw'].append(KF_norms)
     plot_dict['KF-ratio'].append(KF_ratios)
     plot_dict['KF-kernel'].append(KF_kers)
@@ -138,50 +135,51 @@ def compute_complexity(X, k=5):
 
 
 def compute_tau(gen_gap, metric, inverse=False):
-  '''
-  Input: array (generalization gap); array (metric computed for the model instance at such a generalization gap); 
-  - If inverse: first take inverse of the metric, and compute the kendall tau coefficient
-  Return: kendall's tau coefficient, pvalue
-  '''
-  if inverse:
-    metric = np.array([1/elem for elem in metric])
-  tau, p_value = stats.kendalltau(gen_gap, metric)
-  return tau, p_value
+    '''
+    Input: array (generalization gap); array (metric computed for the model instance at such a generalization gap); 
+    - If inverse: first take inverse of the metric, and compute the kendall tau coefficient
+    Return: kendall's tau coefficient, pvalue
+    '''
+    if inverse:
+        metric = np.array([1/elem for elem in metric])
+    tau, p_value = stats.kendalltau(gen_gap, metric)
+    return tau, p_value
 
 
 
 def get_df_tau(plot_dict, gen_err):
-  '''
-  Return a dataframe of the kendall tau's coefficient for different methods
-  '''
-  #tau, p_value = compute_tau(result_dict[err], plot_dict['avg_clusters'], inverse=True)
-  #taus, pvalues, names, inverses = [tau], [p_value], ['cc'], ['True']
-  taus, pvalues, names, inverses = [], [], [], []
-  for key, value in plot_dict.items():
-    value = np.array(value)
-    # if key in ['ranks', 'stable_ranks', 'avg_clusters', 'modularity']:
-    #   continue
-    for i in range(value.shape[1]):
-      if key == 'Schatten':
-        if i == 0: #Schatten 1-norm, no inversion
-          inverse_flag = False
-        elif i == 1:
-          continue #skip trivial 2-norm
-        else:
-          inverse_flag = True
-      else:
-        inverse_flag = True
-      tau, p_value = compute_tau(gen_err, value[:,i], inverse=inverse_flag)
-      taus.append(tau)
-      pvalues.append(p_value)
-      names.append(key + '_' +str(i+1))
-      inverses.append(inverse_flag)
+    '''
+    Return a dataframe of the kendall tau's coefficient for different methods
+    '''
+    #tau, p_value = compute_tau(result_dict[err], plot_dict['avg_clusters'], inverse=True)
+    #taus, pvalues, names, inverses = [tau], [p_value], ['cc'], ['True']
+    taus, pvalues, names, inverses = [], [], [], []
+    for key, value in plot_dict.items():
+        value = np.array(value)
+        # if key in ['ranks', 'stable_ranks', 'avg_clusters', 'modularity']:
+        #   continue
+        for i in range(value.shape[1]):
+            if key == 'Schatten':
+                if i == 0: #Schatten 1-norm, no inversion
+                    inverse_flag = False
+                elif i == 1:
+                    continue #skip trivial 2-norm
+                else:
+                    inverse_flag = True
+            else:
+                inverse_flag = True
+        tau, p_value = compute_tau(gen_err, value[:,i], inverse=inverse_flag)
+        taus.append(tau)
+        pvalues.append(p_value)
+        names.append(key + '_' +str(i+1))
+        inverses.append(inverse_flag)
 
-  kendal_cor = pd.DataFrame(
-    {'metric': names,
-     'kendall_tau': taus,
-     'pvalue': pvalues,
-     'inverse': inverses
-    })
+    kendal_cor = pd.DataFrame(
+        {
+            'metric': names,
+            'kendall_tau': taus,
+            'pvalue': pvalues,
+            'inverse': inverses
+        })
 
-  return kendal_cor
+    return kendal_cor
