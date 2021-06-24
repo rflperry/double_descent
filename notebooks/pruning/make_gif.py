fd88fd0618868820406196082a25d51bbb13cf80
rflperry <rflperry@gmail.com>
2021-06-24 15:14:52 -0400

    gif epochs

--- notebooks/pruning/make_gif.py
+++ notebooks/pruning/make_gif.py
@@ -4,17 +4,18 @@ from PIL import Image
 
 # filepaths
 
-name = "spiral_layers=2_heatmap"#
-fp_in = f"./results/{name}*.png"
+direc = './results/spiral_training_epochs/'
+name = "./spiral_layers=2_training_hyperplanes"#
+fp_in = f"{direc}{name}*.png"
 fp_out = f"./reports/{name}.gif"
 
 filepaths = glob.glob(fp_in)
 # print(fp_in[:fp_in.index("*")] + r"([0-9]+)*.png")
 sorted_filepaths = sorted(
     filepaths, key = lambda s: int(re.match(fp_in[:fp_in.index("*")] + r".*?([0-9]+)\.png", s).group(1))
-)[::-1]
+)#[::-1]
 
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 img, *imgs = [Image.open(f) for f in sorted_filepaths]
 img.save(fp=fp_out, format='GIF', append_images=imgs,
-         save_all=True, duration=len(imgs)*20, loop=0)
+         save_all=True, duration=200, loop=0)
